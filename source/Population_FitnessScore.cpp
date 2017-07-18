84329098391ab072d42378f0f463300526106182
Tony T. Liang <ltong1130@gmail.com>
2017-07-17 18:39:35 -0700

    modified get_fitness_score() to add the 3rd basic constraint and the large-xlarge advanced constraint, if this turns out to be wrong, go back to the last version

--- source/Population_FitnessScore.cpp
+++ source/Population_FitnessScore.cpp
@@ -14,6 +14,8 @@ double Population::get_fitness_score(void) {
 	// # of the case that does not fit the professors' preference time_slot
 	int prefer = 0;
 
+	//  compare class volume and room capacity
+	int rCapFit = 0;
 
 
 	// # of total rooms avaliable
@@ -49,6 +51,7 @@ double Population::get_fitness_score(void) {
 
 	for (int i = 0; i<(int)_chromosome.size(); i++) {
 
+		// --------------------- series_Overlap ---------------------------------
 		// compute the # of courses of same field scheduled in the same time slot
 		int course_id = class_id_to_type(_chromosome[i]._class_id);
 		if (course_id >= 3000 && course_id < 6000) {
@@ -73,7 +76,7 @@ double Population::get_fitness_score(void) {
 
 
 
-
+		// ------------------------------- multi_c ---------------------------------------
 		// check if the same professor has multiple courses to teach in the same semester
 		// if she does, then check if multiple different courses have been scheduled at the 
 		// same time_slot for her. 
@@ -98,12 +101,15 @@ double Population::get_fitness_score(void) {
 				time_room[_chromosome[i]._time_slot].insert(_chromosome[i]._room);
 		}
 
+
+		// ---------------------------------- prefer --------------------------------------------------
 		// check the professors' preference 
 		unordered_set<int> preference = get_prof_preference(_chromosome[i]._professor_id);
 		if (preference.size() > 1 && preference.find(_chromosome[i]._time_slot) == preference.end())
 			prefer += 1;
 
 
+		// ----------------------------- roomSizeConstraints ------------------------------------------
 		// large & xlarge roomSize constraints
 		if (249 >= get_room_cap_by_index(_chromosome[i]._room) && get_room_cap_by_index(_chromosome[i]._room) >= 125) {
 			if (time_largeRoom.find(_chromosome[i]._time_slot) == time_largeRoom.end()) {
@@ -125,11 +131,22 @@ double Population::get_fitness_score(void) {
 				time_extraRoom[_chromosome[i]._time_slot].insert(_chromosome[i]._class_id);
 			}
 		}
+		
+		// -------------------------------- rCapFit -----------------------------------------------
+		// return the num of students in this class
+		int classVolume = getStudNum_by_classIdx(_chromosome[i]._class_id);
+		// return the num of avaliable seats in this room 
+		int roomCap = get_room_cap_by_index(_chromosome[i]._room);
+		if (classVolume>roomCap) {
+			rCapFit += 1;
+		}
 
 	}
-
 	// large & xlarge roomSize constraints
 	int roomSizeConstraints = (largeNum > largeMax ? (largeNum - largeMax) : 0) + (extraNum > extraMax ? (extraNum - extraMax) : 0);
+
+
+	
 
 
 	/*#########################################################################*/
@@ -143,7 +160,7 @@ double Population::get_fitness_score(void) {
 
 
 	// if fScore == 1, then consider the fitness of professors' preference
-	fScore = (1.0 / (Ovrlap + multi_c + roomSizeConstraints + 1.0)) < 1.0 ? (1.0 / (Ovrlap + multi_c + roomSizeConstraints + 1.0)) : (1.0 + 1.0 / (1 + prefer + series_Overlap));
+	fScore = (1.0 / (Ovrlap + multi_c + rCapFit + 1.0)) < 1.0 ? (1.0 / (Ovrlap + multi_c + rCapFit + 1.0)) : (1.0 + 1.0 / (1 + prefer + series_Overlap + roomSizeConstraints));
 
 	// linear fitness Score function
 	// fScore = 1.0 - double(Ovrlap + multi_c) / double(2*row);
